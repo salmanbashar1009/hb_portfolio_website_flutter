aa26afdf8c49fb3787f9e92e8fc3ab0fb1a0c500
salmanbashar1009 <salmanbashar101@gmail.com>
2025-09-20 18:09:40 +0600

    widget tester added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,24 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hb_website/ui/screens/home_page.dart';
+import 'package:hb_website/ui/components/desktop/intro_desktop.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomePage());
+void main(){
+  testWidgets("HB portfolio intro renders correctly", (WidgetTester tester) async{
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child:IntroDesktop(),
+          ),
+        ),
+      )
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    /// verify static text
+    expect(find.text("Hi, I'm"),findsOneWidget);
+    expect(find.text("Habibul Bashar"), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    /// verify description
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
